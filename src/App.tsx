e5ca0796fac27e5d64e7972dcb8015aefe8ef772
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-11 10:19:10 +0000

    Hello Saturday

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
+export type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState<Movie[]>(moviesFromServer);
+
+  const handleInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    const newQuery = inputValue.trim();
+
+    setQuery(inputValue);
+
+    setVisibleMovies(
+      moviesFromServer.filter(
+        movie =>
+          movie.title.toLowerCase().includes(newQuery.toLowerCase()) ||
+          movie.description.toLowerCase().includes(newQuery.toLowerCase()),
+      ),
+    );
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +46,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={event => handleInputValue(event)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
